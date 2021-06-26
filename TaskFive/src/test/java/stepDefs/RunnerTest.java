45329deb2bfeecbb422295eae7dc7b0d427a050b
Viktor2001vasilev <Viktor2001vasilev@yandex.ru>
2021-06-26 10:48:46 +0300

    VasilevViktorAleksandrovich_CucumberHomeWork

--- TaskFive/src/test/java/stepDefs/RunnerTest.java
+++ TaskFive/src/test/java/stepDefs/RunnerTest.java
@@ -1,11 +1,12 @@
+package stepDefs;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/java/features"},
-        glue = {"src/test/java/stepDefs"}
-
+        features = {"src/test/java"}
 )
 
 public class RunnerTest extends AbstractTestNGCucumberTests {
+
 }
--- TaskFive/src/test/java/stepDefs/stepsWithAvito.java
+++ TaskFive/src/test/java/stepDefs/stepsWithAvito.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
+
 public class stepsWithAvito {
     public ChromeDriver driver;
     public WebDriverWait explicitWait;
@@ -31,7 +32,6 @@ public class stepsWithAvito {
     public void close() {
         driver.quit();
     }
-
 
     @Пусть("открыт ресурс авито")
     public void openSite() {
